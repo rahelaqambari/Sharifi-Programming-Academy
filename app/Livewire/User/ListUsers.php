d8c71cecbf114234bb944c64b5fca31f697c59b3
rahelaqambari <rahelaqambari@gmail.com>
2026-05-13 21:55:45 +0430

    changes

--- app/Livewire/User/ListUsers.php
+++ app/Livewire/User/ListUsers.php
@@ -8,6 +8,7 @@ use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use Filament\Schemas\Contracts\HasSchemas;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
@@ -28,6 +29,9 @@ class ListUsers extends Component implements HasActions, HasSchemas, HasTable
             ->query(fn (): Builder => User::query())
             ->columns([
                 //
+                TextColumn::make('name')->searchable(),
+                TextColumn::make('email'),
+                TextColumn::make('role')->badge(),
             ])
             ->filters([
                 //
