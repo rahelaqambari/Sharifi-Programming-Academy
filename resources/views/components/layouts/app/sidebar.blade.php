5acaef02d859490e3619287e5f8f86c15572572e
rahelaqambari <rahelaqambari@gmail.com>
2026-05-13 21:27:27 +0430

    livewire component

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -2,6 +2,13 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         @include('partials.head')
+        <style>
+            [x-cloak] {
+                display: none !importent;
+            }
+            @filamentStyles
+            @vite('resources/css/app.css');
+            </style>
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:sidebar sticky stashable class="border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
@@ -136,5 +143,8 @@
         {{ $slot }}
 
         @fluxScripts
+
+        @filamentScripts
+        @vite('resources/js/app.js')
     </body>
 </html>
