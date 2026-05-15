d7ea3b2b641bba606abac8a7be53fee326756f79
rahelaqambari <rahelaqambari@gmail.com>
2026-05-15 22:44:24 +0430

    new changes

--- app/Livewire/Student/ListStudents.php
+++ app/Livewire/Student/ListStudents.php
@@ -8,6 +8,7 @@ use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use Filament\Schemas\Contracts\HasSchemas;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
@@ -28,6 +29,11 @@ class ListStudents extends Component implements HasActions, HasSchemas, HasTable
             ->query(fn (): Builder => Student::query())
             ->columns([
                 //
+                TextColumn::make('user.name'),
+                TextColumn::make('user.email'),
+                TextColumn::make('last_name'),
+                TextColumn::make('tazkira'),
+                TextColumn::make('phone'),
             ])
             ->filters([
                 //
