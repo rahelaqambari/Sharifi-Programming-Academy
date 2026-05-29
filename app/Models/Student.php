c9c1f6e008b553ccd1219d176b0115f8223313b6
rahelaqambari <rahelaqambari@gmail.com>
2026-05-29 21:36:39 +0430

    fillable

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     //
+    protected $fillable = [
+        'last_name',
+        'img_url',
+        'phone',
+        'tazkira',
+        'user_id',
+    ];
         public function user(){
         return $this->belongsTo(User::class);
     }
