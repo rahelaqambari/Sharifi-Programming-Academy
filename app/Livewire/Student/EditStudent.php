5becde8ec38d43e728e5ecaa8d337b1636f180f3
rahelaqambari <rahelaqambari@gmail.com>
2026-06-01 21:42:33 +0430

    section is added to update the student

--- app/Livewire/Student/EditStudent.php
+++ app/Livewire/Student/EditStudent.php
@@ -5,6 +5,8 @@ namespace App\Livewire\Student;
 use App\Models\Student;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use Filament\Schemas\Contracts\HasSchemas;
 use Filament\Schemas\Schema;
@@ -30,6 +32,16 @@ class EditStudent extends Component implements HasActions, HasSchemas
         return $schema
             ->components([
                 //
+                 Section::make('Edit Student Info')
+                ->description('You Can Edit The Spicefic Student Information .')
+                ->columns(2)
+                ->schema([
+                    TextInput::make('Last_name')->label('Course Name'),
+                    TextInput::make('img_url')->format('Y-m-d')->timezone('Asia/Kabul'),
+                    TextInput::make('phone')->format('Y-m-d')->timezone('Asia/Kabul'),
+                    TextInput::make('tazkira')->autosize(),
+                    TextInput::make('user_id')->autosize(),
+            ])                
             ])
             ->statePath('data')
             ->model($this->record);
