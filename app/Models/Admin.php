34540f96e8f381ccad8c352504acaa7305b03d46
rahelaqambari <rahelaqambari@gmail.com>
2026-06-05 21:04:37 +0430

    changes o admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     //
+    protected $fillable = [
+        'user_id',
+        'bio',
+        'last_name',
+        'img_url',
+    ];
 }
